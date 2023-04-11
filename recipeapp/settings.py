a419e18098d6f69c087689b8e23121cde875ed6c
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-04-11 17:09:39 +0200

    Update

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'media'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
